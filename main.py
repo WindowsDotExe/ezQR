244b6fa8fb201b1af4237277decb92cb105db042
Matthew <mjacobs@ualberta.ca>
2022-02-07 20:15:14 -0700

    Added short docstring for generate()

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # This is a very simple QR code generator program.
-# A QR code is generated in the current director with the information provided.
+# A QR code is generated in the current director with the information provided.-
 
 import qrcode
 
 def generate():
+    '''
+    Generates a QR code based on text (str) input.
+    .jpg image created in directory of execution.
+    '''
     url = input('Enter text to generate a QR code: ')
     file = input('Enter the file for the QR code or press enter for default ("qr.jpg"): ')
 
